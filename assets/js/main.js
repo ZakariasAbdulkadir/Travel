9cff84cdf7bf134d6793eb88b9d0b9e6e48947e6
ZakariasAbdulkadir <z.k.abdulkadir@gmail.com>
2021-05-13 16:35:38 -0600

    Added styling to text and pictures

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,3 +12,36 @@ const showMenu = (toggleId, navId) =>{
 showMenu('nav-toggle', 'nav-menu');
 
 
+
+const btnOpenVideo = document.querySelectorAll(".islands__video-content");
+const islandsPopup = document.getElementById('popup');
+
+function popUp() {
+    islandsPopup.classList.add('show-popup');
+}
+
+btnOpenVideo.forEach(btn => btn.addEventListener('click', popUp));
+
+const btnCloseVideo = document.getElementById('popup-close');
+
+btnCloseVideo.addEventListener("click", () => {
+    islandsPopup.classList.remove('show-popup');
+})
+
+
+// Swiper
+
+const galleryThumbs = new Swiper(".gallery-thumbs", {
+    spaceBetween: 0,
+    slidesPerView: 0,
+})
+
+const galleryTop = new Swiper(".gallery-top", {
+    thumbs: {
+      swiper: swiper,
+    }
+})
+
+
+
+
